6b9973eaa38d76a1412a70cef2efe02abb724719
Jared Smith <smith@utk.edu>
2015-05-09 20:33:59 -0400

    basic working example of a map with pings

---
+++ harvester.py
@@ -0,0 +1,8 @@
+import tweepy
+import sys
+import pika
+import json
+import time
+
+if "__name__" == "__main__":
+    pass
--- server.py
+++ server.py
@@ -5,9 +5,16 @@ app = Flask(__name__)
 @app.route('/')
 def viz():
     viz_css_url = url_for('static', filename='viz.css')
-    return render_template('viz.html', viz_css=viz_css_url)
+    leaflet_css_url = url_for('static', filename='leaflet.css')
+    d3_js_url = url_for('static', filename='d3.min.js')
+    leaflet_js_url = url_for('static', filename='leaflet.js')
+    leaflet_d3_url = url_for('static', filename='leaflet-d3.min.js')
+
+    return render_template('viz.html', viz_css=viz_css_url,
+                           leaflet_css=leaflet_css_url, d3_js=d3_js_url,
+                           leaflet_d3=leaflet_d3_url, leaflet_js=leaflet_js_url)
 
 
-if __name__ == '__main__':
+if __name__ == '__main__'
     app.debug = True
     app.run()
