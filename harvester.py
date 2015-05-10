608a452a3e27045f079efde5f292393712f0c8db
Jared Smith <smith@utk.edu>
2015-05-10 16:54:15 -0400

    added basic sentiment analyzer, added configurable tweet filtering

--- harvester.py
+++ harvester.py
@@ -14,17 +14,22 @@ import time
 # 3rd Party assets
 import tweepy
 import pika
+from textblob import TextBlob
 
 # Project assets
 import credentials
+from sentiment import SentimentAnalyzer
 
 
 # Embedded class for the stream listener that will pull from the Twitter Streaming API
 class CustomStreamListener(tweepy.StreamListener):
 
     # Initialize the class
-    def __init__(self, api):
+    def __init__(self, api, filter_type):
         self.api = api
+        self.filter_type = filter_type
+        self.analyzer = SentimentAnalyzer()
+
         super(tweepy.StreamListener, self).__init__()
 
         # Setup RabbitMQ Connection
@@ -41,12 +46,25 @@ class CustomStreamListener(tweepy.StreamListener):
     # When a status is recieved, this will execute
     def on_status(self, status):
 
-        #print status.text + "\n"
+        # Get the sentiment score and assign the correct color to it.
+        # Default to grey for neutral, and we don't have to check
+        # if it returns neutral.
+        def get_sentiment():
+            color = 'grey'
+            sentiment = self.analyzer.trivially_analyze(status.text)
+            if sentiment == 'pos':
+                color = 'green'
+            elif sentiment == 'neg':
+                color = 'red'
+            return color
 
         data = {}
-        if status.coordinates is not None:
+        if status.coordinates is not None and status.coordinates['type'] == 'Point':
+            color = get_sentiment()
+
             data['lon'] = status.coordinates['coordinates'][0]
             data['lat'] = status.coordinates['coordinates'][1]
+            data['color'] = color
         else:
             pass
 
@@ -70,16 +88,25 @@ class CustomStreamListener(tweepy.StreamListener):
 class Harvester(object):
 
     # Initialize the class
-    def __init__(self):
+    def __init__(self, filter_type='location', location=[-125.06,24.57,-67.3,49.03], keywords=None):
+        self.filter_type = filter_type
+        self.location = location
+        self.keywords = keywords
         self.auth = tweepy.OAuthHandler(credentials.CONSUMER_KEY, credentials.CONSUMER_SECRET)
         self.auth.set_access_token(credentials.ACCESS_TOKEN, credentials.ACCESS_TOKEN_SECRET)
         self.api = tweepy.API(self.auth)
-        self.stream_listener = CustomStreamListener(self.api)
+        self.stream_listener = CustomStreamListener(self.api, self.filter_type)
         self.streaming_api = tweepy.streaming.Stream(self.auth, self.stream_listener)
 
     def stream(self):
-        #print 'Starting the stream...'
+        print 'Starting the stream...'
 
-        # TODO: Let this be customizable, i.e. take a bounding box from the web interface
-        # Filter by location using a bounding box on the contiguous united states.
-        self.streaming_api.filter(locations=[-125.06,24.57,-67.3,49.03])
+        if self.filter_type is 'location':
+            # TODO: Let this be customizable, i.e. take a bounding box from the web interface
+            # Filter by location using a bounding box on the contiguous united states.
+            self.streaming_api.filter(locations=location)
+        elif self.filter_type is 'keyword':
+            # Filter by keywords
+            self.streaming_api.filter(keywords=keywords)
+
+
